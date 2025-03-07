d5a8d88cacf8ca662a968984c5e6be2f4b663aa6
Krishna Tanwar <kstanwar0000@gmail.com>
2025-03-07 22:47:15 +0530

    Operators in Java

---
+++ 7. Operators/assignmentOperator.java
@@ -0,0 +1,19 @@
+public class assignmentOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        a+= 5;
+
+        int b = 15;
+        b-=5;
+        System.out.println(b);
+        
+        b*=5;
+        System.out.println(b);
+        b/=5;
+        System.out.println(b);
+        b%=5;
+        
+        System.out.println(b);
+    }
+    
+}
