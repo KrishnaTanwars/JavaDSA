f360a323a3e6fcb6400b5f1f4e2bd4149f023355
Krishna Tanwar <kstanwar0000@gmail.com>
2025-03-08 23:16:30 +0530

    conditional basics

---
+++ 8. Conditional Statements/ifElse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ifElse{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.println("Can Vote & Drive");
+        }
+        if(age>=13 && age<=18){
+            System.out.println("Teenager");
+
+        }
+        else{
+            System.out.println("Not Vote & Drive");
+        }
+
+    }
+}
