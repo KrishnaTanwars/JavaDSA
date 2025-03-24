4bfd1b72dcc6db71bbafff87e55510186c668834
Krishna Tanwar <kstanwar0000@gmail.com>
2025-03-24 21:46:24 +0530

    updated code

--- 14. Advanced Patterns (Part - 2)/hollowRectangle.java
+++ 14. Advanced Patterns (Part - 2)/hollowRectangle.java
@@ -1,6 +1,24 @@
 public class hollowRectangle {
-    public static void main(String[] args) {
+    public static void hollowRec(int row, int col) {
+        //OUTER LOOP
+        for(int i = 1; i<= row;i++){
+            //INNER COLUMNS
+            for(int j = 1; j<=col;j++){
+                //CELL
+                if(i == 1|| i == row || j == 1 || j == col){
+                    //BOUNDARY CELLS
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
         
+    }
+    public static void main(String[] args) {
+        hollowRec(5, 4);
     }
     
 }
